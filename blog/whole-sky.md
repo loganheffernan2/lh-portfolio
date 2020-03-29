72042f7f21db2128d857b6c81d6a41332932539a
Logan Heffernan <loganheffernan@logans-mbp.lan>
2020-03-28 19:54:11 -0500

    Add image.

--- blog/whole-sky.md
+++
@@ -1,6 +0,0 @@
----
-year: '2020'
-title: Whole Sky
-image: assets/quick.png
----
-This is a project that I'm including as an example.
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,15 +1,21 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Header from '../components/Header'
+import '../css/item.css'
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+    <div className="content">
+      <div><Header /></div>
+      <div id="blogWrap">
+        <p id="postTitle">
+        <p>{frontmatter.title}</p>
+        <span> — </span>
+        <p>{frontmatter.date}</p>
+        </p>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
@@ -23,7 +29,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "YYYY")
         path
         title
       }
